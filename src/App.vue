9cf81bc13af7bee0776b9c7f0ad70517209dbf91
Paulo Henrique Lopes Prado <paulohenprado@gmail.com>
2020-09-20 07:11:20 -0300

    modifed doc

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <WeatherMain />
   </div>
 </template>
@@ -16,13 +15,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/WeatherMain.vue
+++ src/components/WeatherMain.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="WeatherMain">
-    <button v-on:click="getWeather(testCity)">Get Weather Data</button>
+    <button v-on:click="getWeather(testCity.lat, testCity.lon)">
+      Get Weather Data
+    </button>
   </div>
 </template>
 
@@ -12,25 +14,39 @@ const openWeatherApiKey = "f1c6f3727e6bebbb08e546ce1dc2236a";
 /**
  * Get weather data from OpenWeatherMaps One Call API
  *
- * @param {Object} location A Object containing lat(latitude) and lon(longitude) float numbers
+ * A Api de one call receive the parameters:
+ * {
+ *  appid: the api key,
+ *  lat: latitude,
+ *  lon: longitude,
+ *  units: the unit of measurement, default being kelvin,
+ *  lang: language, defaults to english
+ * }
+ *
+ * @param {String} lat the selected location latitude
+ * @param {String} lon the selected longitude
  * @param {String} apiKey The OpenWeatherMaps ApiKey
  */
-const getCurrentWeather = (location, apiKey) => {
+
+const getCurrentWeather = (lat, lon, apiKey) => {
   return axios({
     method: "get",
     url: "https://api.openweathermap.org/data/2.5/onecall",
     params: {
       appid: apiKey,
-      lat: location.lat,
-      lon: location.lon,
+      lat: lat,
+      lon: lon,
+      units: "metric",
+      lang: "en",
     },
   })
     .then((response) => {
-      console.log(response);
+      console.log(response.data);
       return response.data;
     })
     .catch((error) => console.log(error));
 };
+
 export default {
   name: "WeatherMain",
   data: function() {
@@ -48,8 +64,8 @@ export default {
     };
   },
   methods: {
-    getWeather: function(location) {
-      getCurrentWeather(location, openWeatherApiKey).then(
+    getWeather: function(lat, lon) {
+      getCurrentWeather(lat, lon, openWeatherApiKey).then(
         (weather) => (this.currentWeather = weather)
       );
     },
